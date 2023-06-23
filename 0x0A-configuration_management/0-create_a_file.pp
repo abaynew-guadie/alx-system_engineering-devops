7cb19b2990ff328e94edee9faf0bdcba76447103
abaynew-guadie <abaynew@yahoo.com>
2023-06-23 15:29:15 +0000

    task 0-2

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
-# Using puppet, create a file with below specifications
-
-file { '/tmp/holberton':
-  ensure  => file,
-  path    => '/tmp/holberton',
+# Using Puppet, create a file in '/tmp'.
+file { '/tmp/school':
+  ensure  => 'file',
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
+  content => 'I love Puppet',
 }
