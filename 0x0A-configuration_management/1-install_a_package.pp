bbfb30299042d371f8d74a746c3f2edc37acc209
abaynew-guadie <abaynew@yahoo.com>
2023-06-23 15:34:32 +0000

    task 0-2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# Read: http://puppet-lint.com/checks/
-# Using Puppet, install puppet-lint.
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+# Instal flask with puppet
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
