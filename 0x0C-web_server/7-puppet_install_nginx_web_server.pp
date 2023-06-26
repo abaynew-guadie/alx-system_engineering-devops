b85bdd447c6f8c88eb42500ab02c4b7998aa79b2
Abaynew Guadie Nigussie <113917030+abaynew-guadie@users.noreply.github.com>
2023-06-26 17:10:45 +0300

    task 0-5

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,6 @@
+#Installing nginx on server using puppet
+exec { 'server configuration':
+   provider => shell,
+	 command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Hello World!" > /var/www/html/index.html; sudo sed -i "/server_name _;/a location /redirect_me {\\n\\treturn 301 https:github.com;\\n\\t}\\n" /etc/nginx/sites-available/default; sudo service nginx restart',
+}
+
